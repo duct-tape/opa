82d5d5f6f0ecacf89f92c95dfc07cb018afbd9cc
Nick Garanko <ngaranko@gmail.com>
2021-03-08 23:40:14 +0100

    Improved command line interface.

--- opa/__init__.py
+++ opa/__init__.py
@@ -1,137 +1,101 @@
 #!/bin/env python
+import base64
 import json
-import os
-import re
 import subprocess
 import click
-import keyring
-import pyperclip
+
+from opa.util import get_list, get_item, print_item, execute
 
 
-SESSION_PREFIX = os.environ.get("OPA_PREFIX", "my")
+ITEM_TEMPLATES = dict(
+    login={
+        'fields': [
+            {'designation': 'username', 'name': 'username', 'type': 'T', 'value': ''},
+            {'designation': 'password', 'name': 'password', 'type': 'P', 'value': ''}
+        ],
+        'notesPlain': '',
+        'passwordHistory': [],
+        'sections': []
+    }
+)
 
 
-@click.command()
-@click.argument("name")
-@click.option("-c",
-              "copy",
-              is_flag=True,
-              help="Silently copy password value into clipboard.")
-@click.option("-l",
-              "list_items",
-              is_flag=True,
-              help="List available items")
-def opa(name, copy, list_items):
-    """1Password helper utility."""
-    if list_items:
-        get_items_list()
-    else:
-        get_item(name, copy)
+@click.group()
+def opa():
+    pass
 
 
-def get_items_list():
-    """List items."""
-    command = "op list items"
-    result = execute(command=command)
-
-    data = json.loads(result)
-    for item in data:
+@opa.command()
+def list():
+    for item in get_list(option="items"):
         print(item["overview"]["title"])
 
 
-def get_item(name, copy):
-    """Get single item information by name or uuid.
-    Copy password to clipboard, if -c flag provided."""
-    command = f'op get item "{name}"'
-    result = execute(command=command)
-
-    if result is None:
-        return
-
-    try:
-        data = json.loads(result)
-    except ValueError:
-        print(repr(result))
-        return
-
-    for field in get_fields(data):
-        value = field.get("v", field.get("value"))
-        name = field.get("t", field.get("name"))
-        if value is not None:
-            if copy:
-                if name == "password":
-                    pyperclip.copy(value)
-            else:
-                print("{}: {}".format(name, value))
+@opa.command()
+@click.argument("name")
+def search(name):
+    for item in get_list(option="items"):
+        title = item["overview"]["title"]
+        if name.lower() in title.lower():
+            print(title)
 
 
-def login():
-    """
-    Perform login operation, ask for password and save session key into keyring
-    """
-    key = keyring.get_password("opa", "key")
-    if key is None:
-        key = click.prompt("Please enter password, blin.")
-        keyring.set_password("opa", "key", key)
+@opa.command()
+@click.argument("name")
+@click.option("-c", "--copy",
+              is_flag=True,
+              help="Silently copy password value into clipboard.")
+def get(name, copy):
+    data = get_item(name)
+    if data is None:
+        exit(1)
 
-    if key is None:
-        raise Exception("Failed to log in.")
-
-    try:
-        output = subprocess.check_output(
-            f'echo "{key}" | op signin {SESSION_PREFIX}',
-            shell=True
-        )
-    except subprocess.CalledProcessError as e:
-        print("Failed to log in.")
-        print(f"  {e}")
-        raise Exception(1)
-
-    session_key = re.findall(
-        r'export OP_SESSION_{}="(?P<key>.*)"'.format(SESSION_PREFIX),
-        output.decode('utf-8'))[0]
-    keyring.set_password("opa", "session", session_key)
-    return session_key
+    print_item(data, copy)
 
 
-def execute(command, **kwargs):
-    """Execute command with session key. Login, if necessary"""
-    session_key = get_session_key()
-    if not session_key:
-        session_key = login()
-
-    env = dict(os.environ, OP_SESSION_my=session_key)
-    result = None
-    try:
-        result = subprocess.check_output(
-            command,
-            shell=True,
-            env=env,
-            stderr=subprocess.STDOUT,
-            **kwargs)
-    except subprocess.CalledProcessError as e:
-        print(e.output.decode("utf-8"))
-        if "You are not currently signed in" in e.output.decode("utf-8"):
-            session_key = login()
-            env = dict(os.environ, OP_SESSION_my=session_key)
-            result = subprocess.check_output(command, shell=True, env=env)
-
-    return result
+@opa.command()
+def vaults():
+    for item in get_list(option="vaults"):
+        print(item['name'])
 
 
-def get_fields(data):
-    """Fetch fields from details or sections"""
-    for field in data["details"].get("fields", []):
-        yield field
+@opa.command()
+@click.argument("name")
+@click.option("-c", "--copy",
+              is_flag=True,
+              help="Silently copy password value into clipboard.")
+@click.option("-r", "--recipe",
+              type=str,
+              help=(
+                  "Recipe for generated password: comma separated list of "
+                  "'letters','digits', 'symbols' and 0-64 for password length."
+              ),
+              default="letters,digits,symbols,32")
+def gen(name, copy, recipe):
+    template = ITEM_TEMPLATES['login']
 
-    sections = data["details"].get("sections", [])
-    for section in sections:
-        for field in section.get("fields", []):
-            yield field
+    position, username_template = [
+        (c, x) for c, x in enumerate(template['fields']) if x['name'] == 'username'
+    ][0]
+    username_template['value'] = name
+    template['fields'][position] = username_template
 
+    data_to_encode = json.dumps(template, separators=(',', ':'))
 
-def get_session_key():
-    return keyring.get_password("opa", "session")
+    p = subprocess.Popen(["op", "encode"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    encoded_output, errors = p.communicate(data_to_encode.encode('utf-8'))
+    if errors is None:
+        encoded_data = encoded_output.decode('utf-8').strip()
+    else:
+        print(f"Failed to encode data: {errors}")
+        exit(1)
+
+    # Create item
+    data = base64.b64encode(json.dumps(template).encode('utf-8')).decode('utf-8')
+    command = f'op create item login {encoded_data} --title="{name}" --generate-password={recipe}'
+    result = json.loads(execute(command=command))
+
+    print_item(data=get_item(result['uuid']), copy=copy)
 
 
 if __name__ == '__main__':
---
+++ opa/util.py
@@ -0,0 +1,128 @@
+import json
+import os
+import re
+import subprocess
+import click
+import keyring
+import pyperclip
+
+
+SESSION_PREFIX = os.environ.get("OPA_PREFIX", "my")
+
+
+def get_list(option):
+    """List items."""
+    result = execute(command=f"op list {option}")
+
+    data = json.loads(result)
+    for item in data:
+        yield item
+
+
+def get_item(name):
+    """Get single item information by name or uuid."""
+    result = execute(command=f'op get item "{name}"')
+
+    if result is None:
+        return None
+
+    try:
+        data = json.loads(result)
+    except ValueError:
+        print(repr(result))
+        return
+
+    return get_values(data)
+
+
+def login(session_suffix):
+    """
+    Perform login operation, ask for password and save session key into keyring
+    """
+    key = keyring.get_password("opa", "key")
+    if key is None:
+        key = click.prompt("Please enter password, blin.")
+        keyring.set_password("opa", "key", key)
+
+    if key is None:
+        raise Exception("Failed to log in.")
+
+    try:
+        output = subprocess.check_output(
+            f'echo "{key}" | op signin {SESSION_PREFIX}',
+            shell=True
+        )
+    except subprocess.CalledProcessError as e:
+        print("Failed to log in.")
+        print(f"  {e}")
+        raise Exception(1)
+
+    session_key = re.findall(
+        r'export OP_SESSION_{}="(?P<key>.*)"'.format(SESSION_PREFIX),
+        output.decode('utf-8'))[0]
+    keyring.set_password("opa", "session", session_key)
+    return session_key
+
+
+def execute(command, **kwargs):
+    """Execute command with session key. Login, if necessary"""
+    session_key = get_session_key()
+    if not session_key:
+        session_key = login()
+
+    env = dict(os.environ, OP_SESSION_my=session_key)
+    result = None
+    try:
+        result = subprocess.check_output(
+            command,
+            shell=True,
+            env=env,
+            stderr=subprocess.STDOUT,
+            **kwargs)
+    except subprocess.CalledProcessError as e:
+        print(e.output.decode("utf-8").strip())
+        if "You are not currently signed in" in e.output.decode("utf-8"):
+            session_key = login()
+            env = dict(os.environ, OP_SESSION_my=session_key)
+            result = subprocess.check_output(command, shell=True, env=env)
+
+    return result
+
+
+def get_fields(data):
+    """Fetch fields from details or sections"""
+    for field in data["details"].get("fields", []):
+        yield field
+
+    sections = data["details"].get("sections", [])
+    for section in sections:
+        for field in section.get("fields", []):
+            yield field
+
+
+def get_values(data):
+    values = dict(
+        title=data["overview"]["title"]
+    )
+
+    for field in get_fields(data):
+        value = field.get("v", field.get("value"))
+        name = field.get("t", field.get("name"))
+        values[name] = value
+
+    return values
+
+def get_session_key():
+    return keyring.get_password("opa", "session")
+
+
+def print_item(data, copy=None):
+    if copy:
+        if data.get('password') is not None:
+            pyperclip.copy(data['password'])
+            print(f"Copied {data['title']} password for {data['username']} into clipboard.")
+        else:
+            print("MEH!", repr(data))
+    else:
+        for key, value in data.items():
+            print(f"{key}: {value}")
